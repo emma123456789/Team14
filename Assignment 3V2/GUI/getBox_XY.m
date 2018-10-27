e446068b77821323a81d8f13b20df10157f9c18a
JialinWang1995 <42365265+JialinWang1995@users.noreply.github.com>
2018-10-27 21:36:24 +1100

    Add files via upload

---
+++ Assignment 3V2/GUI/getBox_XY.m
@@ -0,0 +1,43 @@
+  function [x,y]= getBox_XY(hObject)
+    % box = imread('no_blocks.jpg');
+    global vid2
+
+    if (get(hObject,'Value') == 1)
+    snapshot = getsnapshot(vid2);
+    box = snapshot;
+
+
+    imshow(box); hold on;
+    box1 = box;
+    box1(:,1:600,:)=0;     %1:580
+    box1(:,1180:1600,:)=0;  %1180:1600
+    box1(710:1200,:,:)=0;   %710:1200
+    greybox1 = rgb2gray(box1);
+    bw = imbinarize(greybox1,0.4);   %0.5
+
+    bw1 = ~bwareaopen(~bw,1000);
+    bw2 = ~bwareaopen(~bw,152000);
+
+    Size1 = length(find(bw1));
+    Size2 = length(find(bw2));
+
+    if (Size1 <Size2)
+        disp('Blocks detected');
+    % detection of box
+    bOrientation = regionprops('table',bw2,'Centroid','Image','Orientation');
+    N= length(bOrientation.Orientation);
+     for i= 1:N 
+      if   (length(bOrientation.Image{i})>200)
+     index=i;
+      end
+     end
+    plot(bOrientation.Centroid(index,1),bOrientation.Centroid(index,2),'*');
+    text(bOrientation.Centroid(index,1),bOrientation.Centroid(index,2),num2str(bOrientation.Orientation(index)),'Color','red','FontSize',20);
+    contour(bw2,'r');
+    else
+        disp('No Blocks');
+    end
+    x=bOrientation.Centroid(index,1);
+    y=bOrientation.Centroid(index,2);
+    end
+    end
