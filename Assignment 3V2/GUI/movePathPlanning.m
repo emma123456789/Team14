a242f7e69600ba2b2bbed2b7dd4e75bc77a9f5ce
Calandra <cal_lun@hotmail.com>
2018-11-09 14:26:31 +1100

    path planning fixed
    String split is working and sending correct x y coordinates to robotstudios

--- Assignment 3V2/GUI/movePathPlanning.m
+++ Assignment 3V2/GUI/movePathPlanning.m
@@ -9,15 +9,15 @@ function movePathPlanning(bpPath)
     grip_height = table_height + block_height;
     
     start = split(bpPath{1,1});
-%     start = num2cells(start);
     [x,y] = gameboardConversion(str2num(start{1}(2)),start{1}(1));
     %move to start block position
     commandStr = sprintf('moveerb %.3f %.3f %.3f %.3f %.3f %.3f %s', x, y,move_height,0,180,0,fast);
     queue.add(commandStr);
 
     
-    %bring down to 5mm above table height and move along path
-    length = size(bpPath{1,:});
+    %bring ee down to 5mm above table height and move along path
+    length = size(bpPath);
+    length = length(2);
     for i = 1:length
         position = split(bpPath{1,i});
         [x,y] = gameboardConversion(str2num(position{1}(2)),position{1}(1));
@@ -29,32 +29,4 @@ function movePathPlanning(bpPath)
     commandStr = sprintf('moveerb %.3f %.3f %.3f %.3f %.3f %.3f %s', x, y,move_height,0,180,0,fast);
     queue.add(commandStr);
     
-    %% to change in StartMaze function
-%     global ppStartLetter ppStartNumber 
-%     global ppGoalLetter ppGoalNumber
-% 
-%     %code to combine letter with number 
-%     ppStart = strcat(ppStartLetter, ppStartNumber);
-%     ppGoal = strcat(ppGoalLetter,ppGoalNumber);
-% 
-%     %display the start and end BPs
-%     disp(ppStart);
-%     disp(ppGoal);
-% 
-%     %does path planning with start and goal gui inputs
-%     ppBpPath = pathPlanning(ppStart, ppGoal);
-% 
-%     noPath = 'Path not available!';
-%     yesPath = 'Path available - moving now!';
-% 
-%     %checks if there is a path available or not
-%     if isempty(ppBpPath)
-%         %prints on gui there is no path
-%         set(handles.PathPlanningText, 'String', noPath);
-%     else
-%         %prints on gui there is a path available
-%         set(handles.PathPlanningText, 'String', yesPath);
-%         %moves robot along path
-%         movePathPlanning(ppBpPath);
-%     end
 end
